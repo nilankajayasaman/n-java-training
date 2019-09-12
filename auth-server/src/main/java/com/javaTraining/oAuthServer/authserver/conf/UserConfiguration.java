03a4a0acf40070942b500d0dc680f567dc126e7c
User <nilanka4@gmail.com>
2019-09-12 12:17:05 +0530

    auth server

---
+++ auth-server/src/main/java/com/javaTraining/oAuthServer/authserver/conf/UserConfiguration.java
@@ -0,0 +1,29 @@
+package com.javaTraining.oAuthServer.authserver.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("nilanka")
+//                .password(passwordEncoder.encode("1234"))
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER","ADMIN","MANAGER")
+                .authorities("CAN_READ","CAN_WRITE","CAN_DELETE")
+                .and()
+                .withUser("krish")
+                .password(passwordEncoder.encode("4321"))
+                .roles("USER")
+                .authorities("CAN_READ","CAN_WRITE");
+
+    }
+}
